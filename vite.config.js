f59a1fddea93d58d9411ff532bb244c2a9ad856c
U22099 <nifemiolaniyi4@gmail.com>
2024-12-13 22:22:52 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,47 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+  registerType: 'autoUpdate',
+  manifest: {
+    name: 'Web Code Editor',
+    short_name: 'Code Editor',
+    description: 'A web code editor powered by monaco editor (same editor powering vscode) with code execution feature for languages ranging from javascript, lua, python, typescript and many more',
+    background_color: 'black',
+    display: 'standalone',
+    start_url: '/webcodeeditor',
+    scope: '/',
+    orientation: 'portrait',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: "pwa-64x64.png",
+        sizes: "64x64",
+        type: "image/png"
+		},
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+		},
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+  		},
+      {
+        src: "maskable-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable"
+  		}
+	]
+  }
+})],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "/src"),
