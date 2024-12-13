698e81caffd564c8c5b7394c9dc4c98a4af342f2
U22099 <nifemiolaniyi4@gmail.com>
2024-12-13 22:31:26 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: "/webcodeeditor/",
   plugins: [react(),
   VitePWA({
   registerType: 'autoUpdate',
